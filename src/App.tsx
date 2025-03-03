94af49c93687890648205b531697a2737dab19e6
Arjav5090 <arjavvirani123@gmail.com>
2025-03-03 13:46:11 -0500

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import ResourcesPage from "./pages/Resources";
@@ -8,11 +8,25 @@ import ContactForm from "./pages/Contact";
 import Forum from "./pages/Forum";
 import Questionnaire from "./pages/Questionnaire";
 import Chatbot from "./pages/Chatbot";
+import ReactGA from "react-ga"; // Use react-ga4 for GA4 support
+
+const TRACKING_ID = "G-M424D013QG"; // Your Google Analytics Measurement ID
+ReactGA.initialize(TRACKING_ID);
+
+// Hook to track page views on route change
+const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+};
 
 const App: React.FC = () => {
   return (
     <Router basename="/">
       <MainLayout>
+        <PageTracker /> {/* Track route changes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/resources/*" element={<ResourcesPage />} />
@@ -21,11 +35,16 @@ const App: React.FC = () => {
           <Route path="/forum" element={<Forum />} />
           <Route path="/chatbot" element={<Chatbot />} />
           <Route path="/questionnaire" element={<Questionnaire />} />
-          {/* Add more routes as needed */}
         </Routes>
       </MainLayout>
     </Router>
   );
+};
+
+// Separate component for page tracking
+const PageTracker = () => {
+  usePageTracking();
+  return null;
 };
 
 export default App;
